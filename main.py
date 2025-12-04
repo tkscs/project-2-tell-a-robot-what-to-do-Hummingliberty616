c58abc5e4e5fc1081392c23c43153df2163ef37f
Hummingliberty616 <wlin2028@kehillahstudent.org>
2025-12-04 09:22:25 -0800

    -

--- main.py
+++ main.py
@@ -5,4 +5,30 @@ from simulator import robot, FORWARD, BACKWARD, STOP
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
 # When you're done, close the simulator
-robot.exit()
+current_state = "idle" 
+
+while True: 
+    if current_state == "idle":
+        current_input = input("What do you want to do?")
+
+    if current_state == "idle":
+        if current_input == "spin clockwise please":
+            while True:
+                robot.right(90)
+                stop = input("Stop?")
+                if stop == "Yes":
+                    break
+        if current_input == "spin clockwise":
+            print("AH AH AH! YOU DIDN'T SAY THE MAGIC WORD!")
+        if current_input == "spin counterclockwise":
+            while True:
+                #Do 
+    
+        if current_input == "Make animal sounds":
+            animal = input("Which animal?")
+            if animal == "dog":
+                #Bark
+            if animal == "cat":
+                #meow
+              
+
